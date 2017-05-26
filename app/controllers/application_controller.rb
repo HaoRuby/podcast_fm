cf6fdddf72183ca905bb43fa73ca3f081d9a1ede
Hao Nguyen <hao@localhost.localdomain>
2017-05-26 09:57:03 +0700

    Added attributes to devise podcast

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_paramters, if: :devise_controller?
+
+  protected
+
+  # config strong params for podcast's additional attributes 
+  def configure_permitted_paramters
+  	devise_parameter_sanitizer.permit(:sign_up) do |u|
+  		u.permit( :email,
+  							:password,
+  							:password_confirmation,
+  							:title)
+  	end
+  	
+  	devise_parameter_sanitizer.permit(:account_update) do |u|
+  		u.permit( :email,
+  							:password,
+  							:password_confirmation,
+  							:current_password,
+  							:title,
+  							:description,
+  							:itunes,
+  							:stitcher,
+  							:podbay)
+  	end
+  end
 end
