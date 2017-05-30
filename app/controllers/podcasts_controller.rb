f2c23eb5337631751a29329fb8e8a3776174c9ee
Hao Nguyen <hao@localhost.localdomain>
2017-05-30 09:17:34 +0700

    show episode

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,9 +1,10 @@
 class PodcastsController < ApplicationController
 	def index
-		@podcasts = Podcast.all.order("created_at DESC");
+		@podcasts = Podcast.all.order("created_at DESC")
 	end
 
 	def show
 		@podcast = Podcast.find(params[:id])
+		@episodes = Episode.where(podcast_id: @podcast.id).order("created_at DESC")
 	end
 end
