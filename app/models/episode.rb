7df46f95adc5b5f3c6cdfb030a9b10baa888c3b4
Hao Nguyen <hao@localhost.localdomain>
2017-05-31 14:54:58 +0700

    changed s3 protocol

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,8 +2,7 @@ class Episode < ApplicationRecord
   belongs_to :podcast
 
   has_attached_file :episode_thumbnail, 
-  									styles: { large: "1000x1000#", medium: "550x550#" },
-  									:s3_protocol => :https 
+  									styles: { large: "1000x1000#", medium: "550x550#" }
   									#default_url: "/images/:style/missing.png"
   validates_attachment_content_type :episode_thumbnail, 
   																	content_type: /\Aimage\/.*\z/
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -7,8 +7,7 @@ class Podcast < ApplicationRecord
   has_many :episodes
 
   has_attached_file :thumbnail, 
-  									styles: { large: "1000x1000#", medium: "550x550#" },
-                    :s3_protocol => :http 
+  									styles: { large: "1000x1000#", medium: "550x550#" }
   									#default_url: "/images/:style/missing.png"
   validates_attachment_content_type :thumbnail, 
   																	content_type: /\Aimage\/.*\z/
