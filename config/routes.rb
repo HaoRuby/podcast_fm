c49849b7bbc99c48be19dd931a1a71e75a9cfb3f
Hao Nguyen <hao@localhost.localdomain>
2017-05-31 08:25:26 +0700

    Added dashboard for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :podcasts
-	root 'welcome#index'
 
 	resources :podcasts, only: [:index, :show] do
 		resources :episodes
-	end 
+	end
+
+	# if you're not sign in, the root will be wellcome#index; else the root will be podcasts#dashboard
+	authenticated :podcast do
+    root 'podcasts#dashboard', as: :authenticated_root
+  end
+  root 'welcome#index' 
 end
